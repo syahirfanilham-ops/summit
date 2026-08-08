5a3e0caec8ce505e4b52856a8bb25222b7649786
syahirfanilham-ops <syahirfanilham@gmail.com>
2026-08-09 02:20:29 +0800

    full flow website

---
+++ tailwind.config.js
@@ -0,0 +1,2 @@
+/** @type {import('tailwindcss').Config} */
+export default { content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'], theme: { extend: { colors: { navy: '#0B1E3D', offwhite: '#FAF8F4', gold: '#D4A62A', orange: '#D46A2A' }, fontFamily: { sans: ['Poppins', 'sans-serif'], serif: ['Playfair Display', 'serif'] } } }, plugins: [] }
